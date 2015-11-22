97501fb3ef06c716eb23cd78a5de056a04c2fa9a
Gilly <egill.ian1@gmail.com>
2015-11-22 23:28:22 +0000

    Basic html
    Basic html/jade for home page, additional jade files for support.
    Primitive banner set on page. Access to ./img folder for server.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Command for allowing server to accesss /img folder and use content
+app.use('/img', express.static(__dirname + '/img'))
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Amateur Geologist Federation' });
 });
+
+/* GET About page */
+router.get('/', function(req, res, next) {
+  res.render('about')
+})
 
 module.exports = router;
